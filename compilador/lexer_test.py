f32dc5f328de299e314f19e49b3d966cf099fdcb
Ernesto Adame <ernesto.adame.cendejas@gmail.com>
2023-11-07 16:54:47 -0600

    Changed tester script to read and write test cases programatically

--- compilador/lexer_test.py
+++ compilador/lexer_test.py
@@ -3,20 +3,19 @@ lexer_test.py
 programa que prueba el funcionamiento de lexer.py
 '''
 import sys
+import os
 from lexer import lexer
 
-# especifica la cantidad de test cases que hay en la carpeta tests/lexer
-no_tests = 1
 # Se puede especificar un test case en particular como argumento
 test_no = str(sys.argv[1]) if len(sys.argv) > 1 else None
-test_files = ['test' + str(i) + '.xcx' for i in range(1, no_tests + 1)]
 
 print("---PROBANDO ANALIZADOR LEXICO---")
 
+# Si se especifica un test case en particular, se prueba solo ese
 if test_no:
     print("TEST no.", test_no)
-    data = open('tests/' + 'test' + test_no + '.xcx', 'r').read()
-    print(data)
+    data = open('tests/lexer/' + 'test' + test_no + '.xcx', 'r').read()
+    # print(data)
     lexer.input(data)
     # Tokenize
     while True:
@@ -24,10 +23,24 @@ if test_no:
         if not tok: 
             break      # No more input
         print(tok)
+# Se itera sobre todos los test cases de la carpeta tests/lexer
 else:
-    for i, test in enumerate(test_files):
-        print("TEST no.", i + 1)
-        data = open('tests/lexer/' + test, 'r').read()
+    # assign directory
+    directory = 'tests/lexer'
+    i = 0
+    wf = open("test_results/lexer/result.txt","w")
+    # iterate over files in that directory
+    for filename in os.listdir(directory):
+        rf = os.path.join(directory, filename)
+        i += 1
+        toks = ""
+        test_no = "TEST no." + str(i)
+        # spacing
+        if i != 1:
+            wf.write( "\n")
+        wf.write(test_no + "\n")
+        print(test_no)
+        data = open(rf, 'r').read()
         lexer.input(data)
         # Tokenize
         while True:
@@ -35,6 +48,11 @@ else:
             if not tok: 
                 break      # No more input
             print(tok)
+            toks += str(tok) + "\n"
+        wf.write(str(toks))
+    wf.close()
+
+
 
 
 # ###
