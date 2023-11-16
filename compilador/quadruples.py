f17a63b006e0a85d81fecaea6ba0ae58e450f0cc
Ernesto Adame <ernesto.adame.cendejas@gmail.com>
2023-11-16 14:53:06 -0600

    Estructura para el manejo de cuadruplos

---
+++ compilador/quadruples.py
@@ -0,0 +1,16 @@
+class Quadruples:
+    def __init__(self):
+        # siguiente cuadruplo a generar
+        self.counter = 0
+        self.list = []
+
+    # funcion para generar un nuevo cuadruplo
+    def gen_quad(self, op, left, right, res):
+        quad = [op, left, right, res]
+        self.list.append(quad)
+        self.counter += 1
+
+    # funcion para completar algun dato vacio de un cuadruplo
+    # index es el dato a completar con val
+    def fill_quad(self, quad, index, val):
+        self.list[quad][index] = val
