927338eb8b193d3e6f3ab78a971b5b4eb7f46c02
Ernesto Adame <ernesto.adame.cendejas@gmail.com>
2023-11-16 01:44:15 -0600

    Version 2 func_dir

--- compilador/func_dir.py
+++ compilador/func_dir.py
@@ -1,19 +1,58 @@
 from vars_table import VarsTable
 
-'''
-    formato de directorio de funciones:
-    {
-        'funcion': [tipo, varsTable]
-    }
-
-'''
-
 class FuncDir:
+    # formato de directorio de funciones:
+    # 
+    # 'nombre': [tipo, varsTable, params, ret],
+    # 
+    #  nombre: puede ser global, const, "funcion"
     def __init__(self):
-        self.dir = {
-            'global': ['void', VarsTable('global')]
-        }
+        self.dir = {}   
 
-    def insert_func(self, func, tipo):
-        if (not self.dir.get(func)):
-            self.dir[func] = [tipo, VarsTable('local')]
+    # agregar funcion a dirFunc
+    def add_func(self, func, tipo):
+        if not self.dir.get(func):
+            self.dir[func] = [tipo, None, [], None]
+        else:
+            raise Exception('Funcion {} ya declarada'.format(func))
+        
+    # agregar tabla de variables a funcion
+    def add_varstable(self, func, scope):
+        if self.dir.get(func):
+            self.dir[func][1] = VarsTable(scope)
+        else:
+            raise Exception('Funcion {} no declarada'.format(func))
+
+    # agregar variable a varstable
+    def add_var(self, func, type, varName):
+        if self.dir.get(func):
+            return self.dir[func][1].add_var(type, varName)
+        else:
+            raise Exception('Funcion {} no declarada'.format(func))
+    
+    # agregar constante a dirFunc
+    def add_const(self, func, type, constVal):
+        # si no existia, crear tabla de constantes 
+        if 'const' not in self.dir.keys():
+            self.dir['const'] = ['void', VarsTable('const'), None, None]
+        else:
+           #  const : varstable.add_const(type, constVal)
+           return self.dir['const'][1].add_const(type, constVal)
+        
+    # agregar parametro a funcion
+    # a su vez agrega el parametro a la varstable de dicha funcion
+    def add_param(self, func, type, param):
+        if self.dir.get(func):
+            self.dir[func][2].append((type, param))
+            #  const : varstable.add_var(type, varName)
+            return self.add_var(func, type, param)
+        else:
+            raise Exception('Funcion {} no declarada'.format(func))
+        
+
+    # saber si existe varsTable en funcion
+    def has_varstable(self, func):
+        if self.dir.get(func):
+            return self.dir[func][1] != None
+        else:
+            raise Exception('Funcion {} no declarada'.format(func))
